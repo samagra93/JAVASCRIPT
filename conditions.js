097ba704860e03afdbe4e94f80e8099facdbc85b
Samagra Gupta <gsamagra93@gmail.com>
2025-06-19 16:32:04 +0530

    Added some files

---
+++ conditions.js
@@ -0,0 +1,14 @@
+let age = 18
+
+if (age>=18)
+{
+    console.log("You can vote!")
+}
+else if(age=0)
+{
+    console.log("Invaild age")
+}
+else
+{
+    console.log("You can not vote")
+}
---
+++ variables.js
@@ -0,0 +1,6 @@
+const name = "Samagra"    
+let age = 18
+let isMale = true
+
+//name = "Rahul"
+console.log(name,age)
